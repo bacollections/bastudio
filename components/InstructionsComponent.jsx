fde8c93fccc27b6622b857b1d18461e8ec31d548
ba studio <94141545+bacollections@users.noreply.github.com>
2023-10-09 17:48:14 +0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -56,23 +56,6 @@ export default function InstructionsComponent() {
 						alt="Alchemy Supercharged"
 					/>
 				</a>
-				<div className={styles.icons_container}>
-					<div>
-						<a
-							href="https://github.com/alchemyplatform/create-web3-dapp"
-							target={"_blank"}
-						>
-							Leave a star on Github
-						</a>
-					</div>
-					<div>
-						<a
-							href="https://twitter.com/AlchemyPlatform"
-							target={"_blank"}
-						>
-							Follow us on Twitter
-						</a>
-					</div>
 				</div>
 			</div>
 		</div>
